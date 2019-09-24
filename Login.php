b96d03940df126d34e6d72f3fb2f157f86793e5e
lixianling <afortunes@sina.com>
2019-09-24 16:51:17 +0800

    commit

---
+++ Login.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/9/24
+ * Time: 16:41
+ */
+class Login
+{
+    public function index(){
+        return 'develop分支上的方法';
+    }
+}
